5384bf27f01c08a65a403553a00b9c90ce3455dc
YevhenS <pobeg1990@gmail.com>
2021-02-22 18:24:53 +0200

    add first test

--- src/main/java/TestYoutube.java
+++ src/main/java/TestYoutube.java
@@ -10,9 +10,11 @@ public class TestYoutube {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.youtube.com/");
         WebElement searchBox = driver.findElement(By.name("search_query"));
-        searchBox.sendKeys("2");
+        searchBox.sendKeys("23");
         searchBox.submit();
-//        driver.quit();
+        driver.manage().window().fullscreen();
+//        driver.findElement(By.xpath("(.//div[@id='contents'])[5]/ytd-video-renderer[2]"));
+        driver.quit();
 
     }
 
